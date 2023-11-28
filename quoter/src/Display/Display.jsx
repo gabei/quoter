8d95e6ce3a97465773f28a35389bdd25312f2880
gabei <gabe1792@gmail.com>
2023-11-28 12:41:04 -0600

    initiated display component and tested local react setup

---
+++ quoter/src/Display/Display.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+//import './Display.scss'
+
+export default class Display extends Component { 
+    render() {
+        return (
+            <div className="display">
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
