2ef3d4fcafbc5d11931d2b6a01ebfedaeec7f429
gabei <gabe1792@gmail.com>
2023-11-30 12:07:15 -0600

    setup API call and response on express server. Ready to send data to client side

--- api/app.js
+++ api/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const fetch = require('fetch');
+const  fetch = require('node-fetch')
 
-app.get('/', (_, res)=> {
-    res.send("API endpoint reached.");
+require('dotenv').config();
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': process.env.API_KEY
+    }
+}
+
+async function callAPI(){
+    const response = await fetch(process.env.API_URL, options);
+    const data = await response.json();
+    return data;
+}
+
+callAPI();
+
+app.get('/quote', (_, res)=> {
+    res.send(callAPI);
 });
 
 app.listen(port, ()=> {
